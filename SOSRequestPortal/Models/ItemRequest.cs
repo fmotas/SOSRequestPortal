42b6f72272ef572894ce966a0c7e350a7de1d65e
Fernando <fmsilva@id.uff.br>
2018-06-10 23:51:35 -0300

    First Commit

---
+++ SOSRequestPortal/Models/ItemRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOSRequestPortal.Models
+{
+    public class ItemRequest
+    {
+		public int RequestID { get; set; }
+		public DateTime Data { get; set; }
+		public string Ticket { get; set; }
+		public string Item { get; set; }
+		public string TipoDeImagem { get; set; }
+		public string Colaborador { get; set; }
+		public string Time { get; set; }
+		public string Gestor { get; set; }
+		public string Requisitante { get; set; }
+		public DateTime HorarioDaRequisicao { get; set; }
+		public DateTime HorarioDaRetirada { get; set; }
+		public string Status { get; set; }
+	}
+}
